23a63564da24c59cb43fa1b7ab0fe8d8fcd5ad26
vivek703 <36715078+vivek703@users.noreply.github.com>
2020-04-19 14:33:52 -0700

    Delete .driver.ko.cmd

--- spin_lock/.driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vivek/Desktop/test/spin_lock/driver.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/vivek/Desktop/test/spin_lock/driver.ko /home/vivek/Desktop/test/spin_lock/driver.o /home/vivek/Desktop/test/spin_lock/driver.mod.o
