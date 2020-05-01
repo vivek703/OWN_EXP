d0ecc5d2987d7856b4989c4b6f532dfb70ac23c8
vivek703 <36715078+vivek703@users.noreply.github.com>
2020-05-01 11:05:48 -0700

    Add files via upload

---
+++ tower.c
@@ -0,0 +1,114 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+/*
+ * Complete the towerBreakers function below.
+ */
+int towerBreakers(int arr_count, int* arr) {
+    /*
+     * Write your code here.
+     */
+    return 0;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* t_endptr;
+    char* t_str = readline();
+    int t = strtol(t_str, &t_endptr, 10);
+
+    if (t_endptr == t_str || *t_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char* arr_count_endptr;
+        char* arr_count_str = readline();
+        int arr_count = strtol(arr_count_str, &arr_count_endptr, 10);
+
+        if (arr_count_endptr == arr_count_str || *arr_count_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        char** arr_temp = split_string(readline());
+
+        int arr[arr_count];
+
+        for (int arr_itr = 0; arr_itr < arr_count; arr_itr++) {
+            char* arr_item_endptr;
+            char* arr_item_str = arr_temp[arr_itr];
+            int arr_item = strtol(arr_item_str, &arr_item_endptr, 10);
+
+            if (arr_item_endptr == arr_item_str || *arr_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+            arr[arr_itr] = arr_item;
+        }
+
+        int result = towerBreakers(arr_count, arr);
+
+        fprintf(fptr, "%d\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
